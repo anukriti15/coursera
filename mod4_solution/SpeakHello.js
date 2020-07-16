21abaa5d4c3474e66882a4c700e260c119486352
Anukriti Singh <44464316+anukriti15@users.noreply.github.com>
2020-07-16 17:26:44 +0530

    Update SpeakHello.js

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -3,7 +3,7 @@
 	helloSpeaker.speak=function(name){}
 	var speakWord = "Hello";
 	helloSpeaker.speak=function() {
-       console.log(speakWord + " " +name);
+       console.log(speakWord + " " +helloSpeaker.name);
 }
 window.helloSpeaker=helloSpeaker;
 })(window);
