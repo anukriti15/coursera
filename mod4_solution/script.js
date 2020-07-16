279bf2656c8dbbc73a225826096c10569a5c9a70
Anukriti Singh <44464316+anukriti15@users.noreply.github.com>
2020-07-16 17:47:50 +0530

    Update script.js

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -7,7 +7,7 @@ for (var i = 0; i < names.length; i++) {
 
   var firstLetter = names[i].charAt(0).toLowerCase();
 
-  if (firstLetter === 'J') {
+  if (firstLetter === 'j') {
     byeSpeaker.speak(names[i]);
   } else {
     helloSpeaker.speak(names[i]);
